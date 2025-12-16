f80576598e51342ca1a28770d4b86b69ce7a8496
Fithi Teklom <fthiteklom@gmail.com>
2025-12-16 19:36:50 +0000

    implemented the function, added tittle on html and a small change on css

--- script.js
+++ script.js
@@ -6,7 +6,39 @@ function setup() {
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+ 
 }
+
+function makePageForEpisodes(episodeList) {
+  const rootElem = document.getElementById("root");
+
+  rootElem.innerHTML = "";
+
+  episodeList.forEach((episode) => {
+    
+    const episodeCard = document.createElement("section");
+
+    const title = document.createElement("h2");
+    title.textContent = episode.name;
+
+    const info = document.createElement("p");
+    info.textContent = `Season ${episode.season} Episode ${episode.number}`;
+
+    const image = document.createElement("img");
+    image.src = episode.image.medium;
+    image.alt = episode.name;
+
+    const summary = document.createElement("div");
+    summary.innerHTML = episode.summary;
+
+    episodeCard.appendChild(title);
+    episodeCard.appendChild(info);
+    episodeCard.appendChild(image);
+    episodeCard.appendChild(summary);
+
+    rootElem.appendChild(episodeCard);
+  });
+}
+
 
 window.onload = setup;
